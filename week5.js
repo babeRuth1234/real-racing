5f5d3eecad82cbbd1b8b9d3cb02d2d5a53dffb83
babeRuth1234 <123479850+babeRuth1234@users.noreply.github.com>
2023-12-09 18:32:08 +0100

    Update week5.js

--- week5.js
+++ week5.js
@@ -121,7 +121,7 @@ metShad.addEventListener("click", function(){
 
 
 // set the date we're counting down to 
-var countDownDate4 = new Date("may 23, 2023 21:31:25").getTime();
+var countDownDate4 = new Date("dec 12, 2023 21:31:25").getTime();
 
 // update the count down every 1 second
 var x =  setInterval(function() {
